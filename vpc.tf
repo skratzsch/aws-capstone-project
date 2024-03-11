c95785503e5d794095dbd23eab93156bb11658cd
skratzsch <skratzsch@yahoo.de>
2024-03-11 16:59:04 +0100

    change vpc name

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "capstone-main" {
 }
 
 resource "aws_subnet" "capstone-subnet-1" {
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.capstone-main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-central-1a"
   tags = {
@@ -17,7 +17,7 @@ resource "aws_subnet" "capstone-subnet-1" {
 }
 
 resource "aws_subnet" "capstone-subnet-2" {
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.capstone-main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-central-1b"
   tags = {
