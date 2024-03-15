34329e96c4462075dc1cc33dc2c88ba25084666d
skratzsch <skratzsch@yahoo.de>
2024-03-15 15:28:52 +0100

    add internet gateway, add route table and association

--- vpc.tf
+++ vpc.tf
@@ -24,3 +24,34 @@ resource "aws_subnet" "capstone-subnet-2" {
     Name = "Capstone-VPC-Subnet-2"
   }
 }
+
+resource "aws_internet_gateway" "capstone-igw" {
+  vpc_id = aws_vpc.capstone-main.id
+
+  tags = {
+    Name = "Capstone-Internet-Gateway"
+  }
+}
+
+resource "aws_route_table" "capstone-rt" {
+  vpc_id = aws_vpc.capstone-main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.capstone-igw.id
+  }
+
+  tags = {
+    Name = "Capstone-Route-Table"
+  }
+}
+
+resource "aws_route_table_association" "capstone-rta-subnet-1" {
+  subnet_id      = aws_subnet.capstone-subnet-1.id
+  route_table_id = aws_route_table.capstone-rt.id
+}
+
+resource "aws_route_table_association" "capstone-rta-subnet-2" {
+  subnet_id      = aws_subnet.capstone-subnet-2.id
+  route_table_id = aws_route_table.capstone-rt.id
+}
